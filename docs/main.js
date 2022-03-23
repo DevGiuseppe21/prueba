bb8f6c80f1672cebe11b1c594cabe24032d33a92
Giuseppe <joseph.grant@mottach.superate.org.pa>
2022-03-22 19:42:36 -0500

    Changelog 1.2

--- docs/main.js
+++ docs/main.js
@@ -1,42 +1,63 @@
 // change navbar styles on scroll
-
 window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
-})
+  document
+    .querySelector('nav')
+    .classList.toggle('window-scroll', window.scrollY > 0);
+});
 
 // show/hide faq answer
 const faqs = document.querySelectorAll('.faq');
 
-faqs.forEach(faq => {
-    faq.addEventListener('click', () => {
-         faq.classList.toggle('open');
-
-         // change icon
-         const icon = faq.querySelector('.faq__icon i');
-         if(icon.className === 'uil uil-plus'){
-              icon.className = "uil uil-minus";
-        } else {
-              icon.className = "uil uil-plus";
-        }
-     })
-})
+faqs.forEach((faq) => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('open');
+    // change icon
+    const icon = faq.querySelector('.faq__icon i');
+    if (icon.className === 'uil uil-plus') {
+      icon.className = 'uil uil-minus';
+    } else {
+      icon.className = 'uil uil-plus';
+    }
+  });
+});
 
 // show/hide nav menu
-const menu = document.querySelector(".nav__menu");
-const menuBtn = document.querySelector("#open-menu-btn");
-const closeBtn = document.querySelector("#close-menu-btn");
+const menu = document.querySelector('.nav__menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
 
 menuBtn.addEventListener('click', () => {
-     menu.style.display = "flex";
-     closeBtn.style.display = "inline-block";
-     menuBtn.style.display = "none"; 
-})
-                                                     
+  menu.style.display = 'flex';
+  closeBtn.style.display = 'inline-block';
+  menuBtn.style.display = 'none';
+});
+
 // close nav menu
-const closeNav = () => { 
-     menu.style.display = "none";
-     closeBtn.style.display = "none";
-     menuBtn.style.display = "inline-block";
-}
-                                    
+const closeNav = () => {
+  menu.style.display = 'none';
+  closeBtn.style.display = 'none';
+  menuBtn.style.display = 'inline-block';
+};
+
 closeBtn.addEventListener('click', closeNav);
+
+// Swiper Cards
+let swiper = new Swiper('.mySwiper', {
+     slidesPerView: 1,
+     spaceBetween: 30,
+     pagination: {
+       el: '.swiper-pagination',
+       clickable: true,
+       background: 'white',
+     },
+     autoplay: {
+       delay: 6500,
+       disableOnInteraction: false,
+     },
+     //  When window width is > = 600px
+     breakpoints: {
+       600: {
+         slidesPerView: 2,
+       },
+     },
+   });
